f783fd8e6458d75f7c59a97fa2f9deaf87b79540
hase111111 <hasehasehase61@gmail.com>
2025-03-18 17:10:48 +0900

    Add: 全盤面を列挙したデータの追加

--- TicTacToe/src/learning.py
+++ TicTacToe/src/learning.py
@@ -1,12 +1,26 @@
 """learning.py"""
 
+import json
+
 import tictactoe.data
 
 
 def main():
     gene = tictactoe.data.BoardEnumerator()
     boards = gene.get_all_boards()
-    print(len(boards))
+
+    dict_boards = dict()
+    for i, board in enumerate(boards):
+        f = tictactoe.data.Field()
+        f.from_data(board)
+        move = f.get_all_available_moves()
+        move_dict = dict()
+        for m in move:
+            move_dict[m] = 100 / len(move)
+        dict_boards[board] = move_dict
+
+    with open("data.json", "w", encoding="utf-8") as f:
+        json.dump(dict_boards, f)
 
 
 if __name__ == "__main__":
